c8cf46e201632234d2944ac79e18a0338e7f5b50
Shreyansh <0902shreyanshg@gmail.com>
2023-03-19 11:24:27 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Container, Row, Col } from "reactstrap";
+import HeroSlider from '../components/UI/HeroSlider'
+import Helmet from '../components/Helmet/Helmet'
+
+const Home = () => {
+  return (
+    <Helmet title="Home">
+      {/* ============= hero section =========== */}
+      <section className="p-0 hero__slider-section">
+        <HeroSlider />
+
+        
+      </section>
+    </Helmet>
+  )
+}
+
+export default Home
